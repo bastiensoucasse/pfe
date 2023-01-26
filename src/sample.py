2fc2f58e511cfe0aa28eb9d5e339268e4254861d
Bastien Soucasse <bastien.soucasse@etu.u-bordeaux.fr>
2023-01-26 14:27:02 +0100

    Update sample extension script documentation

--- src/sample.py
+++ src/sample.py
@@ -1,10 +1,10 @@
 '''
 To run this sample script, start Slicer with the following arguments.
-    --python-script "$(pwd)/src/sample.py".
+    --python-script "path/to/sample.py".
 
-For instance:
-    - On Apple macOS: open /path/to/Slicer.app --args --python-script "$(pwd)/src/sample.py".
-    - On a Linux distribution: ./path/to/Slicer --python-script "$(pwd)/src/sample.py".
+For instance, from the project root folder:
+    - On Apple macOS: open Slicer.app --args --python-script "$(pwd)/src/sample.py".
+    - On a Linux distribution: .Slicer --python-script "$(pwd)/src/sample.py".
 
 A typical Slicer window will open with the main drop-down list containing a Hello World action.
 Clicking this action will open a message box saying “Hello, World!”.
