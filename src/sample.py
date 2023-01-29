850163e72d9682cdfef25cb8bfc1546870b169cf
Bastien Soucasse <bastien.soucasse@etu.u-bordeaux.fr>
2023-01-29 10:44:33 +0100

    Update the tutorial for a manual run

--- src/sample.py
+++ src/sample.py
@@ -1,13 +1,16 @@
 '''
-To run this sample script, start Slicer with the following arguments.
-    --python-script "path/to/sample.py".
-
-For instance, from the project root folder:
-    - On Apple macOS: open Slicer.app --args --python-script "$(pwd)/src/sample.py".
-    - On a Linux distribution: .Slicer --python-script "$(pwd)/src/sample.py".
+Sample script for a Hello World extension which adds to Slicer a Hello World module, opening a
+Hello World dialog when triggered.
 
 A typical Slicer window will open with the main drop-down list containing a Hello World action.
 Clicking this action will open a message box saying “Hello, World!”.
+
+To run this sample script manually, start Slicer with the following arguments.
+    --python-script "path/to/sample.py".
+
+For instance, from the project root folder:
+    - On Apple macOS: open /path/to/Slicer.app --args --python-script "$(pwd)/src/sample.py".
+    - On a Linux distribution: /path/to/Slicer --python-script "$(pwd)/src/sample.py".
 '''
 
 from typing import TYPE_CHECKING, Any
