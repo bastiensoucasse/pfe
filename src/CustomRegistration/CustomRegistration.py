682003178589d4f36c5db013b8b16eb273ede30c
Bastien Soucasse <bastien.soucasse@etu.u-bordeaux.fr>
2023-02-06 08:55:11 +0100

    Implement empty UI

--- src/CustomRegistration/CustomRegistration.py
+++ src/CustomRegistration/CustomRegistration.py
@@ -2,14 +2,17 @@
 The Custom Registration module for Slicer provides the features for 3D images registration, based on the ITK library.
 """
 
-import qt
-import slicer
-from slicer.ScriptedLoadableModule import ScriptedLoadableModule, ScriptedLoadableModuleLogic, ScriptedLoadableModuleWidget
+from slicer import util
+from slicer.ScriptedLoadableModule import (
+    ScriptedLoadableModule,
+    ScriptedLoadableModuleLogic,
+    ScriptedLoadableModuleWidget,
+)
 
 
 class CustomRegistration(ScriptedLoadableModule):
     """
-    Main class for the Custom Registration module used to define the module‘s metadata.
+    Main class for the Custom Registration module used to define the module's metadata.
     """
 
     def __init__(self, parent):
@@ -24,14 +27,21 @@ class CustomRegistration(ScriptedLoadableModule):
         self.parent.title = "CustomRegistration"
         self.parent.categories = ["PFE"]
         self.parent.dependencies = []
-        self.parent.contributors = ["Wissam Boussella (Université de Bordeaux)", "Iantsa Provost (Université de Bordeaux)", "Bastien Soucasse (Université de Bordeaux)", "Tony Wolff (Université de Bordeaux)"]
+        self.parent.contributors = [
+            "Wissam Boussella (Université de Bordeaux)",
+            "Iantsa Provost (Université de Bordeaux)",
+            "Bastien Soucasse (Université de Bordeaux)",
+            "Tony Wolff (Université de Bordeaux)",
+        ]
         self.parent.helpText = "The Custom Registration module for Slicer provides the features for 3D images registration, based on the ITK library."
-        self.parent.acknowledgementText = "Supported by Fabien Machin (Université de Bordeaux)."
+        self.parent.acknowledgementText = (
+            "Supported by Fabien Machin (Université de Bordeaux)."
+        )
 
 
 class CustomRegistrationLogic(ScriptedLoadableModuleLogic):
     """
-    Logic class for the Custom Registration module used to define the module‘s algorithms.
+    Logic class for the Custom Registration module used to define the module's algorithms.
     """
 
     def __init__(self):
@@ -40,7 +50,7 @@ class CustomRegistrationLogic(ScriptedLoadableModuleLogic):
 
 class CustomRegistrationWidget(ScriptedLoadableModuleWidget):
     """
-    Widget class for the Custom Registration module used to define the module‘s panel interface.
+    Widget class for the Custom Registration module used to define the module's panel interface.
     """
 
     def __init__(self, parent=None):
@@ -51,7 +61,9 @@ class CustomRegistrationWidget(ScriptedLoadableModuleWidget):
         Sets up the widget for the module by adding a welcome message to the layout.
         """
 
+        # Initialize setup from Slicer.
         ScriptedLoadableModuleWidget.setup(self)
 
-        welcome_label = qt.QLabel("Welcome to my custom extension.")
-        self.layout.addWidget(welcome_label)
+        # Load UI file.
+        self.panel_ui = util.loadUI(self.resourcePath("UI/Panel.ui"))
+        self.layout.addWidget(self.panel_ui)
