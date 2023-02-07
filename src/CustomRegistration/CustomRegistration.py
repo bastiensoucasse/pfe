b219a3febdb6d36be11771c150a8fa83642feee3
Iantsa Provost <iantsa.provost@gmail.com>
2023-02-07 12:31:16 +0100

    Volume data & coordinates input retrieving

--- src/CustomRegistration/CustomRegistration.py
+++ src/CustomRegistration/CustomRegistration.py
@@ -2,7 +2,9 @@
 The Custom Registration module for Slicer provides the features for 3D images registration, based on the ITK library.
 """
 
-from slicer import util
+import slicer
+from qt import QPushButton, QSpinBox, QComboBox
+from slicer import util, mrmlScene
 from slicer.ScriptedLoadableModule import (
     ScriptedLoadableModule,
     ScriptedLoadableModuleLogic,
@@ -61,9 +63,80 @@ class CustomRegistrationWidget(ScriptedLoadableModuleWidget):
         Sets up the widget for the module by adding a welcome message to the layout.
         """
 
-        # Initialize setup from Slicer.
+        # :COMMENT: Initialize setup from Slicer.
         ScriptedLoadableModuleWidget.setup(self)
 
-        # Load UI file.
+        # :COMMENT: Load UI file.
         self.panel_ui = util.loadUI(self.resourcePath("UI/Panel.ui"))
         self.layout.addWidget(self.panel_ui)
+
+        # :COMMENT: Get the crop button widget.
+        self.crop_button = self.panel_ui.findChild(QPushButton, "crop_button")
+        self.crop_button.clicked.connect(self.crop)
+
+        # :COMMENT: Get the image combobox widget.
+        self.volumeComboBox = self.panel_ui.findChild(QComboBox, "volume")
+        self.volumeComboBox.activated.connect(self.onVolumeActivated)
+
+        # :COMMENT: Add the available volumes and options to the combobox.
+        # :GLITCH: List doesn't update automatically.
+        volumes = mrmlScene.GetNodesByClass("vtkMRMLScalarVolumeNode")
+
+        # :BUG: Supposed to be a placeholder, but still appears in list (shouldn't)
+        self.volumeComboBox.insertItem(0, "Select a Volume")
+
+        for i in range(volumes.GetNumberOfItems()):
+            volume = volumes.GetItemAsObject(i)
+            self.volumeComboBox.addItem(volume.GetName())
+
+        self.volumeComboBox.addItem("Rename current Volume")
+        self.volumeComboBox.addItem("Delete current Volume")
+
+        # :COMMENT: Add observer to update combobox when new volume is added to MRML Scene
+        self.observerTag = mrmlScene.AddObserver(slicer.vtkMRMLScene.NodeAddedEvent, self.updateCombobox)
+
+
+    def crop(self):
+        # :COMMENT: Print name, ID and dimensions of selected volume.
+        volumes = mrmlScene.GetNodesByClass("vtkMRMLScalarVolumeNode")
+        volume = volumes.GetItemAsObject(self.currentVolumeIndex-1)
+        volumeData = volume.GetImageData()
+        print("Current volume name:", volume.GetName())
+        print("Current volume index:", self.currentVolumeIndex)
+        print("Dimensions:", volumeData.GetDimensions(), "\n")
+
+        # :COMMENT: Retrieve coordinates input and print them.
+        start_x = self.panel_ui.findChild(QSpinBox, "sx").value
+        start_y = self.panel_ui.findChild(QSpinBox, "sy").value
+        start_z = self.panel_ui.findChild(QSpinBox, "sz").value
+        end_x = self.panel_ui.findChild(QSpinBox, "ex").value
+        end_y = self.panel_ui.findChild(QSpinBox, "ey").value
+        end_z = self.panel_ui.findChild(QSpinBox, "ez").value
+        print("Start X:", start_x)
+        print("Start Y:", start_y)
+        print("Start Z:", start_z)
+        print("End X:", end_x)
+        print("End Y:", end_y)
+        print("End 2:", end_z)
+
+    def onVolumeActivated(self, index):
+        options = ["Select a Volume", "Delete current Volume", "Rename current Volume"]
+
+        name = self.volumeComboBox.currentText
+        if name in options:
+            # :TODO: Add renaming and deleting features.
+            print(name, "not implemented yet!\n")
+
+        else:
+            self.currentVolumeIndex = index
+            # :TODO: Set min and max for coordinates
+
+    def updateCombobox(self, caller, event):
+        volumes = mrmlScene.GetNodesByClass("vtkMRMLNode")
+        volumesNames = []
+        for i in range(volumes.GetNumberOfItems()):
+            volume = volumes.GetItemAsObject(i)
+            if volume.GetName() != "":
+                volumesNames.append(volume.GetName())
+        self.comboBox.clear()
+        self.comboBox.addItems(volumesNames)
