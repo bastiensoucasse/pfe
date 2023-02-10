f60e2830852f11a781cb93ad2ccb1d289085b7df
Iantsa Provost <iantsa.provost@gmail.com>
2023-02-10 11:36:52 +0100

    Added generation of unique volume name & a bit of code factorization

--- src/CustomRegistration/CustomRegistration.py
+++ src/CustomRegistration/CustomRegistration.py
@@ -1,11 +1,13 @@
 """
 The Custom Registration module for Slicer provides the features for 3D images registration, based on the ITK library.
 """
-import SimpleITK as sitk
+import datetime
+
 import numpy as np
+import SimpleITK as sitk
 import vtk
-from qt import QPushButton, QSpinBox, QComboBox, QMessageBox, QLabel
-from slicer import util, mrmlScene, vtkMRMLScalarVolumeNode
+from qt import QComboBox, QInputDialog, QLabel, QMessageBox, QPushButton, QSpinBox, QDialog, QLineEdit
+from slicer import mrmlScene, util, vtkMRMLScalarVolumeNode
 from slicer.ScriptedLoadableModule import (
     ScriptedLoadableModule,
     ScriptedLoadableModuleLogic,
@@ -73,7 +75,6 @@ class CustomRegistrationWidget(ScriptedLoadableModuleWidget):
 
         self.crop_setup()
 
-
     def crop_setup(self):
         # :COMMENT: Get the crop button widget.
         self.crop_button = self.panel_ui.findChild(QPushButton, "crop_button")
@@ -101,14 +102,12 @@ class CustomRegistrationWidget(ScriptedLoadableModuleWidget):
         # :BUG: List not updated when new volume loaded.
         # self.observerTag = mrmlScene.AddObserver(slicer.vtkMRMLScene.NodeAddedEvent, self.updateCombobox)
 
-        # :COMMENT: Get the coordinates spinbox widgets.
-        # :DIRTY: To be factorized
-        self.sx = self.panel_ui.findChild(QSpinBox, "sx")
-        self.sy = self.panel_ui.findChild(QSpinBox, "sy")
-        self.sz = self.panel_ui.findChild(QSpinBox, "sz")
-        self.ex = self.panel_ui.findChild(QSpinBox, "ex")
-        self.ey = self.panel_ui.findChild(QSpinBox, "ey")
-        self.ez = self.panel_ui.findChild(QSpinBox, "ez")
+        # :COMMENT: Get the coordinates entered by user in spinbox widgets.
+        self.start = []
+        self.end = []
+        for i in ['x', 'y', 'z']:
+            self.start.append(self.panel_ui.findChild(QSpinBox, "s" + i))
+            self.end.append(self.panel_ui.findChild(QSpinBox, "e" + i))
 
     def onVolumeActivated(self, index):
         options = ["Select a Volume", "Delete current Volume", "Rename current Volume"]
@@ -116,27 +115,30 @@ class CustomRegistrationWidget(ScriptedLoadableModuleWidget):
         name = self.volumeComboBox.currentText
         if name in options:
             # :TODO: Add renaming and deleting features.
-            print("[DEBUG]", name, "not implemented yet!\n")
+            if name == "Delete current Volume":
+                print("[DEBUG]", name, "not implemented yet!\n")
+
+            if name == "Rename current Volume":
+                print("[DEBUG]", name, "not implemented yet!\n")
+                # self.renameVolume()
+
             self.currentVolumeIndex = -1
 
         else:
             self.currentVolumeIndex = index
-            self.currentVolume = self.volumes.GetItemAsObject(index-1)
+            self.currentVolume = self.volumes.GetItemAsObject(index - 1)
             volume_data = self.currentVolume.GetImageData()
             self.currentVolumeDim = volume_data.GetDimensions()
             print("[DEBUG] Volume Dimensions:", self.currentVolumeDim)
             self.update_volume_dimensions()
 
-            # :DIRTY: To be factorized
-            self.sx.setMaximum(self.currentVolumeDim[0])
-            self.sy.setMaximum(self.currentVolumeDim[1])
-            self.sz.setMaximum(self.currentVolumeDim[2])
-            self.ex.setMaximum(self.currentVolumeDim[0])
-            self.ey.setMaximum(self.currentVolumeDim[1])
-            self.ez.setMaximum(self.currentVolumeDim[2])
+            for i in range(3):
+                self.start[i].setMaximum(self.currentVolumeDim[i])
+                self.end[i].setMaximum(self.currentVolumeDim[i])
 
     def crop(self):
         # :COMMENT: Do not do anything if crop button clicked but no volume selected.
+        # :TODO: Solve, knowing that it works for the "Select a Volume" case.
         if self.currentVolumeIndex < 0:
             return
 
@@ -144,32 +146,26 @@ class CustomRegistrationWidget(ScriptedLoadableModuleWidget):
         print("[DEBUG] Current volume index:", self.currentVolumeIndex)
 
         # :COMMENT: Retrieve coordinates input.
-        # :DIRTY: To be factorized.
-        start_x = self.sx.value
-        start_y = self.sy.value
-        start_z = self.sz.value
-        end_x = self.ex.value
-        end_y = self.ey.value
-        end_z = self.ez.value
-        print("[DEBUG] Start X:", start_x)
-        print("[DEBUG] Start Y:", start_y)
-        print("[DEBUG] Start Z:", start_z)
-        print("[DEBUG] End X:", end_x)
-        print("[DEBUG] End Y:", end_y)
-        print("[DEBUG] End Z:", end_z, "\n")
-        start = [start_x, start_y, start_z]
-        end = [end_x, end_y, end_z]
+        start_val = []
+        end_val = []
+        for i in range(3):
+            start_val.append(self.start[i].value)
+            end_val.append(self.end[i].value)
 
         # :COMMENT: Check that coordinates are valid.
-        if (end[0] < start[0]) or (end[1] < start[1]) or (end[2] < start[2]):
-            self.error_message("End values must be greater than or equal to start values.")
+        if any(end_val[i] < start_val[i] for i in range(3)):
+            self.error_message(
+                "End values must be greater than or equal to start values."
+            )
             return
 
         # :COMMENT: Get the selected volume and convert it to a SimpleITK image.
         selected_volume = self.currentVolume
         # print("[DEBUG]", type(selected_volume))
         vtk_image = selected_volume.GetImageData()
-        np_array = vtk.util.numpy_support.vtk_to_numpy(vtk_image.GetPointData().GetScalars())
+        np_array = vtk.util.numpy_support.vtk_to_numpy(
+            vtk_image.GetPointData().GetScalars()
+        )
         np_array = np.reshape(np_array, (vtk_image.GetDimensions()[::-1]))
         # print("[DEBUG]", type(np_array))
         sitk_image = sitk.GetImageFromArray(np_array)
@@ -177,22 +173,21 @@ class CustomRegistrationWidget(ScriptedLoadableModuleWidget):
         print("[DEBUG] Size:", sitk_image.GetSize())
 
         # :COMMENT: Get the size of the crop region.
-        start = [self.sx.value, self.sy.value, self.sz.value]
-        end = [self.ex.value, self.ey.value, self.ez.value]
-        size = [end[0]-start[0]+1, end[1]-start[1]+1, end[2]-start[2]+1]
+        size = [end_val[i] - start_val[i] + 1 for i in range(3)]
 
         # :COMMENT: Crop the image.
         extract_filter = sitk.ExtractImageFilter()
         extract_filter.SetSize(size)
-        extract_filter.SetIndex(start)
+        extract_filter.SetIndex(start_val)
         cropped_image = extract_filter.Execute(sitk_image)
         # print("[DEBUG]", type(cropped_image))
         print("[DEBUG] Size:", cropped_image.GetSize())
 
-        # Create a new volume node
+        # :COMMENT: Create a new volume node
         new_volume_node = vtkMRMLScalarVolumeNode()
-        new_volume_node.SetName(selected_volume.GetName() + "_cropped")
-        # :TODO: Change the name of the new volume.
+        current_time = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+        new_name = f"{selected_volume.GetName()}_cropped_{current_time}"
+        new_volume_node.SetName(new_name)
 
         # :COMMENT: Convert the cropped SimpleITK image back to a NumPy array
         np_array = sitk.GetArrayFromImage(cropped_image)
@@ -204,7 +199,7 @@ class CustomRegistrationWidget(ScriptedLoadableModuleWidget):
         vtk_array = vtk.util.numpy_support.numpy_to_vtk(np_array.flatten())
         vtk_image.GetPointData().SetScalars(vtk_array)
 
-        # Update the MRML scene
+        # :COMMENT: Update the MRML scene
         mrmlScene.AddNode(new_volume_node)
         new_volume_node.SetAndObserveImageData(vtk_image)
 
@@ -232,7 +227,13 @@ class CustomRegistrationWidget(ScriptedLoadableModuleWidget):
 
     def update_volume_dimensions(self):
         dim_label = self.panel_ui.findChild(QLabel, "dim_label")
-        dim_label.setText("{} x {} x {}".format(self.currentVolumeDim[0], self.currentVolumeDim[1], self.currentVolumeDim[2]))
+        dim_label.setText(
+            "{} x {} x {}".format(
+                self.currentVolumeDim[0],
+                self.currentVolumeDim[1],
+                self.currentVolumeDim[2],
+            )
+        )
 
     def error_message(self, message):
         msg = QMessageBox()
@@ -240,3 +241,49 @@ class CustomRegistrationWidget(ScriptedLoadableModuleWidget):
         msg.setText(message)
         msg.setWindowTitle("Error")
         msg.exec_()
+
+    # :BUG: None of these versions are working.
+    # def rename_volume(self):
+    #     volume = self.volumes.GetItemAsObject(self.currentVolumeIndex)
+    #     new_name, ok = QInputDialog.getText(
+    #         self,
+    #         "Rename Volume",
+    #         "Enter the new name for the volume:",
+    #         text=volume.GetName(),
+    #     )
+    #     if ok and new_name != "":
+    #         volume.SetName(new_name)
+
+    # def rename_volume(self):
+    #     print(self.currentVolumeIndex)
+    #     self.input_dialog = QInputDialog(None)
+    #     self.input_dialog.setWindowTitle('Rename Volume')
+    #     self.input_dialog.setLabelText('Enter new name:')
+    #     self.input_dialog.setModal(True)
+    #     self.input_dialog.finished.connect(self.handleRenameVolume)
+    #     self.input_dialog.show()
+
+    # def handleRenameVolume(self, result):
+    #     print(self.currentVolumeIndex)
+    #     if result == QDialog.Accepted:
+    #         new_name = self.input_dialog.textValue()
+    #         print(self.currentVolumeIndex)
+    #         selected_volume = self.volumes.GetItemAsObject(self.currentVolumeIndex)
+    #         print(self.currentVolumeIndex)
+    #         print(selected_volume)
+    #         selected_volume.SetName(new_name)
+
+    # def rename_volume(self):
+        # new_name, ok = QInputDialog.getText(
+        #     self, "Rename Volume", "Enter new volume name:", QLineEdit.Normal
+        # )
+        # if ok:
+        #     selected_index = self.volumeComboBox.currentIndex()
+        #     if selected_index > 0:
+        #         selected_volume = self.volumes.GetItemAsObject(selected_index - 1)
+        #         selected_volume.SetName(new_name)
+        #         self.volumeComboBox.setItemText(selected_index, new_name)
+        #         self.currentVolumeIndex = selected_index
+        # selected_index = self.volumeComboBox.currentIndex
+        # selected_volume = self.volumes.GetItemAsObject(selected_index)
+        # print(selected_volume)
