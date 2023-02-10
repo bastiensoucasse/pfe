b53357d7654ca6052a2d2d76ae05593ebd3b3b01
Bastien Soucasse <bastien.soucasse@etu.u-bordeaux.fr>
2023-02-10 18:13:59 +0100

    Add plugin scripts support

--- src/CustomRegistration/CustomRegistration.py
+++ src/CustomRegistration/CustomRegistration.py
@@ -15,11 +15,13 @@ class CustomRegistration(ScriptedLoadableModule):
     Main class for the Custom Registration module used to define the module's metadata.
     """
 
-    def __init__(self, parent):
+    def __init__(self, parent) -> None:
         ScriptedLoadableModule.__init__(self, parent)
+
+        # Set the module metadata.
         self.set_metadata()
 
-    def set_metadata(self):
+    def set_metadata(self) -> None:
         """
         Sets the metadata for the module, i.e., the title, categories, contributors, help text, and acknowledgement text.
         """
@@ -33,10 +35,8 @@ class CustomRegistration(ScriptedLoadableModule):
             "Bastien Soucasse (Université de Bordeaux)",
             "Tony Wolff (Université de Bordeaux)",
         ]
-        self.parent.helpText = "The Custom Registration module for Slicer provides the features for 3D images registration, based on the ITK library."
-        self.parent.acknowledgementText = (
-            "This project is supported by Fabien Baldacci."
-        )
+        self.parent.helpText = "This module provides features for 3D images registration, based on the ITK library."
+        self.parent.acknowledgementText = "This project is supported and supervised by the reseacher and professor Fabien Baldacci (Université de Bordeaux)."
 
 
 class CustomRegistrationLogic(ScriptedLoadableModuleLogic):
@@ -44,8 +44,33 @@ class CustomRegistrationLogic(ScriptedLoadableModuleLogic):
     Logic class for the Custom Registration module used to define the module's algorithms.
     """
 
-    def __init__(self):
+    def __init__(self) -> None:
         ScriptedLoadableModuleLogic.__init__(self)
+
+    def run(self, script_file: str, function_name: str, *args, **kwargs):
+        """
+        Loads and runs a script file.
+
+        Parameters:
+            script_file: The script file.
+            function_name: The name of the function to run.
+            args: Positional arguments to pass.
+            kwargs: Keyword arguments to pass.
+
+        Returns:
+            The result returned.
+        """
+
+        # Load the script file.
+        with open(script_file, "r") as f:
+            code = f.read()
+            exec(code, globals())
+
+        # Retrieve the function.
+        function = globals()[function_name]
+
+        # Run the function.
+        return function(*args, **kwargs)
 
 
 class CustomRegistrationWidget(ScriptedLoadableModuleWidget):
@@ -53,10 +78,10 @@ class CustomRegistrationWidget(ScriptedLoadableModuleWidget):
     Widget class for the Custom Registration module used to define the module's panel interface.
     """
 
-    def __init__(self, parent=None):
+    def __init__(self, parent=None) -> None:
         ScriptedLoadableModuleWidget.__init__(self, parent)
 
-    def setup(self):
+    def setup(self) -> None:
         """
         Sets up the widget for the module by adding a welcome message to the layout.
         """
@@ -64,6 +89,12 @@ class CustomRegistrationWidget(ScriptedLoadableModuleWidget):
         # Initialize setup from Slicer.
         ScriptedLoadableModuleWidget.setup(self)
 
+        # Initialize the logic of the module.
+        self.logic = CustomRegistrationLogic()
+
         # Load UI file.
         self.panel_ui = util.loadUI(self.resourcePath("UI/Panel.ui"))
         self.layout.addWidget(self.panel_ui)
+
+        # :DEBUG: Run the resampling test.
+        # self.logic.run(self.resourcePath("Scripts/Resample.py"), "test_resample")
