112568dc398724d2543b3274a738a7eb7a553007
tony-wolff <tony-wolff@laposte.net>
2023-02-10 11:13:52 +0100

    first custom registration working

--- src/CustomRegistration/CustomRegistration.py
+++ src/CustomRegistration/CustomRegistration.py
@@ -1,7 +1,8 @@
 """
 The Custom Registration module for Slicer provides the features for 3D images registration, based on the ITK library.
 """
-import vtk, qt, ctk, slicer
+import vtk, qt, ctk, slicer, numpy as np
+import SimpleITK as sitk
 from slicer import util, mrmlScene
 from slicer.ScriptedLoadableModule import (
     ScriptedLoadableModule,
@@ -83,8 +84,119 @@ class CustomRegistrationWidget(ScriptedLoadableModuleWidget):
         self.button_registration.clicked.connect(self.rigid_registration)
 
 
+    def vtk2sitk(self, vtkimg):
+        np_array = vtk.util.numpy_support.vtk_to_numpy(vtkimg.GetPointData().GetScalars())
+        np_array = np.reshape(np_array, (vtkimg.GetDimensions()[::-1]))
+        sitk_image = sitk.GetImageFromArray(np_array)
+        sitk_image = sitk.Cast(sitk_image, sitk.sitkFloat32)
+        return sitk_image
+
+    # from github : https://github.com/dave3d/dicom2stl/blob/main/utils/sitk2vtk.py
+    def sitk2vtk(self, sitkimg):
+        size = list(sitkimg.GetSize())
+        origin = list(sitkimg.GetOrigin())
+        spacing = list(sitkimg.GetSpacing())
+        ncomp = sitkimg.GetNumberOfComponentsPerPixel()
+        direction = sitkimg.GetDirection()
+
+        # there doesn't seem to be a way to specify the image orientation in VTK
+
+        # convert the SimpleITK image to a numpy array
+        i2 = sitk.GetArrayFromImage(sitkimg)
+
+        vtk_image = vtk.vtkImageData()
+
+        # VTK expects 3-dimensional parameters
+        if len(size) == 2:
+            size.append(1)
+
+        if len(origin) == 2:
+            origin.append(0.0)
+
+        if len(spacing) == 2:
+            spacing.append(spacing[0])
+
+        if len(direction) == 4:
+            direction = [
+                direction[0],
+                direction[1],
+                0.0,
+                direction[2],
+                direction[3],
+                0.0,
+                0.0,
+                0.0,
+                1.0,
+            ]
+
+        vtk_image.SetDimensions(size)
+        vtk_image.SetSpacing(spacing)
+        vtk_image.SetOrigin(origin)
+        vtk_image.SetExtent(0, size[0] - 1, 0, size[1] - 1, 0, size[2] - 1)
+
+        if vtk.vtkVersion.GetVTKMajorVersion() < 9:
+            print("Warning: VTK version <9.  No direction matrix.")
+        else:
+            vtk_image.SetDirectionMatrix(direction)
+
+        depth_array = vtk.util.numpy_support.numpy_to_vtk(i2.ravel())
+        depth_array.SetNumberOfComponents(ncomp)
+        vtk_image.GetPointData().SetScalars(depth_array)
+
+        vtk_image.Modified()
+        return vtk_image
+
+
     def rigid_registration(self):
-        print("[DEBUG] test")
+        # :COMMENT: if no image is selected
+        if self.fixed_image_combo_box.currentIndex == 0 or self.moving_image_combo_box.currentIndex == 0:
+            print("[DEBUG]: no volume selected !")
+            return
+
+        fixed_image_index = self.fixed_image_combo_box.currentIndex
+        moving_image_index = self.moving_image_combo_box.currentIndex
+        
+        # :COMMENT: itk volume
+        self.fixedVolumeData = self.volumes.GetItemAsObject(fixed_image_index-1).GetImageData()
+        self.movingVolumeData = self.volumes.GetItemAsObject(moving_image_index-1).GetImageData()
+
+        # :COMMENT: conversion to sitk volumes
+        fixed_image = self.vtk2sitk(self.fixedVolumeData)
+        moving_image = self.vtk2sitk(self.movingVolumeData)
+
+        # :COMMENT: FROM simpleitk docs : https://simpleitk.readthedocs.io/en/master/link_ImageRegistrationMethod1_docs.html
+        # a simple 3D rigid registration method
+        R = sitk.ImageRegistrationMethod()
+        R.SetMetricAsMeanSquares()
+        R.SetOptimizerAsRegularStepGradientDescent(4.0, 0.01, 200)
+        initial_transform = sitk.CenteredTransformInitializer(fixed_image, 
+                                                      moving_image, 
+                                                      sitk.Euler3DTransform(), 
+                                                      sitk.CenteredTransformInitializerFilter.GEOMETRY)
+        R.SetInitialTransform(initial_transform)
+        R.SetInterpolator(sitk.sitkLinear)
+        final_transform = R.Execute(fixed_image, moving_image)
+
+        print("-------")
+        print(final_transform)
+        print(f"Optimizer stop condition: {R.GetOptimizerStopConditionDescription()}")
+        print(f" Iteration: {R.GetOptimizerIteration()}")
+        print(f" Metric value: {R.GetMetricValue()}")
+        
+        moving_resampled = sitk.Resample(moving_image, fixed_image, final_transform, sitk.sitkLinear, 0.0, moving_image.GetPixelID())
+        volumeNode=slicer.mrmlScene.AddNewNodeByClass("vtkMRMLScalarVolumeNode")
+        volumeNode.SetName(f"output_volume")
+        itk_moved_volume = self.sitk2vtk(moving_resampled)
+        volumeNode.SetAndObserveImageData(itk_moved_volume)
+        volumeNode.SetSpacing(moving_resampled.GetSpacing())
+        volumeNode.SetOrigin(moving_resampled.GetSpacing())
+        slicer.util.setSliceViewerLayers(volumeNode, fit=True)
+        print(f"[DEBUG]: new volume {volumeNode.GetName()} created !")
+
+
+
+
+
 
 
 
