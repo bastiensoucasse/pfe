a2103d4b1a4d697f84b1388557a971baefa1981c
Bastien Soucasse <bastien.soucasse@etu.u-bordeaux.fr>
2023-02-13 16:37:52 +0100

    Add first documentation

--- src/CustomRegistration/Resources/Scripts/ROISelection.py
+++ src/CustomRegistration/Resources/Scripts/ROISelection.py
@@ -6,8 +6,19 @@ import SimpleITK as sitk
 
 
 def select_roi(image: sitk.Image, threshold: float = 0.5):
+    """
+    Selects ROI from an image.
+
+    Parameters:
+        image: The input image.
+        threshold (optional): The threshold value. The default is 0.5.
+    """
     raise NotImplementedError
 
 
 def test_select_roi():
+    """
+    Tests the ROI selection function.
+    """
+
     raise NotImplementedError
